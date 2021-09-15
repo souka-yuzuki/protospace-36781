82442acdcd34cc606334e10d4552382181db61ab
souka <blackbeanmame@msn.com>
2021-09-15 16:12:06 +0900

    createアクションにデータ保存の記述を書く

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,6 +8,17 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    
+    Prototype.create(prototyp_params)
+    if prototype.create(prototyp_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prototyp_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
